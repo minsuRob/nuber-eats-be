560eff42d75ffc9f0b43274720be97f40f2f363d
minsu.lee <minsu.rob@gmail.com>
2024-08-21 17:45:37 +0900

    5.5: 유저 생성 서비스 및 resolver 추가

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -10,16 +10,19 @@ export class UsersService {
     @InjectRepository(User) private readonly users: Repository<User>
   ) {}
 
-  async createAccount({ email, password, role }: CreateAccountInput) {
+  async createAccount({
+    email,
+    password,
+    role,
+  }: CreateAccountInput): Promise<string | undefined> {
     try {
       const exists = await this.users.findOne({ email });
-
       if (exists) {
-        return;
+        return "There is a user with that email already";
       }
-
       await this.users.save(this.users.create({ email, password, role }));
-      return true;
-    } catch (e) {}
+    } catch (e) {
+      return "Couldn't create account";
+    }
   }
 }
