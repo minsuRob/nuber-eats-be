cd772de320b2c9062ee34d1ce27a345e80d8844a
minsu.lee <minsu.rob@gmail.com>
2024-08-30 18:17:23 +0900

    etc: fix error

--- src/users/dtos/edit-profile.dto.ts
+++ src/users/dtos/edit-profile.dto.ts
@@ -1,11 +1,11 @@
-import { ObjectType, PartialType, PickType } from '@nestjs/graphql';
+import { InputType, ObjectType, PartialType, PickType } from '@nestjs/graphql';
 import { CoreOutput } from 'src/common/dtos/output.dto';
 import { User } from '../entities/user.entity';
 
 @ObjectType()
 export class EditProfileOutput extends CoreOutput {}
 
-@ObjectType()
+@InputType()
 export class EditProfileInput extends PartialType(
   PickType(User, ['email', 'password']),
 ) {}
