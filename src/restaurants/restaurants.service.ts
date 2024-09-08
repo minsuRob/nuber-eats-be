f1074e475b2ee002d370d08ba8d73b1785f6b4ea
minsu.lee <minsu.rob@gmail.com>
2024-09-08 21:21:18 +0900

    11.9: repo class를 통한 구현, 큰 차이는 아직 알아야겠음
    카테고리를 저장하고, 카테고리가 null로 되지않게 처리.

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -12,17 +12,17 @@ import {
   EditRestaurantInput,
   EditRestaurantOutput,
 } from './dtos/edit-restaurant.dto';
+import { CategoryRepository } from './repositories/category.repository';
 
 @Injectable()
 export class RestaurantService {
   constructor(
     @InjectRepository(Restaurant)
     private readonly restaurants: Repository<Restaurant>,
-    @InjectRepository(Category)
-    private readonly categories: Repository<Category>,
+    private readonly categories: CategoryRepository,
   ) {}
 
-  async getOrCreateCategory(name: string): Promise<Category> {
+  async getOrCreate(name: string): Promise<Category> {
     const categoryName = name.trim().toLowerCase();
     const categorySlug = categoryName.replace(/ /g, '-');
     let category = await this.categories.findOne({ slug: categorySlug });
@@ -43,7 +43,7 @@ export class RestaurantService {
       const newRestaurant = this.restaurants.create(createRestaurantInput);
       newRestaurant.owner = owner;
 
-      const category = await this.getOrCreateCategory(
+      const category = await this.categories.getOrCreate(
         createRestaurantInput.categoryName,
       );
       newRestaurant.category = category;
@@ -77,6 +77,20 @@ export class RestaurantService {
           error: "You can't edit a restaurant that you don't own",
         };
       }
+
+      let category: Category = null;
+      if (editRestaurantInput.categoryName) {
+        category = await this.categories.getOrCreate(
+          editRestaurantInput.categoryName,
+        );
+      }
+      await this.restaurants.save([
+        {
+          id: editRestaurantInput.restaurantId,
+          ...editRestaurantInput,
+          ...(category && { category }),
+        },
+      ]);
 
       return {
         ok: true,
