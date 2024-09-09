587c71a6a71fe838cfb5124067c34fc1f91c8bc6
minsu.lee <minsu.rob@gmail.com>
2024-09-09 14:57:56 +0900

    etc: 누락 코드 추가

--- src/common/dtos/pagination.dto.ts
+++ src/common/dtos/pagination.dto.ts
@@ -1,4 +1,4 @@
-import { Field, InputType, Int } from '@nestjs/graphql';
+import { Field, InputType, Int, ObjectType } from '@nestjs/graphql';
 import { CoreOutput } from './output.dto';
 
 @InputType()
@@ -6,6 +6,8 @@ export class PaginationInput {
   @Field(type => Int, { defaultValue: 1 })
   page: number;
 }
+
+@ObjectType()
 export class PaginationOutput extends CoreOutput {
   @Field(type => Int, { nullable: true })
   totalPages?: number;
